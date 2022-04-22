2e53212eb5c596980e3f1101f00688bb519616d9
chrisrw300 <chrisrw300@gmail.com>
2022-04-22 17:43:43 -0600

    started beginning of gunction, design

---
+++ assets/script.js
@@ -0,0 +1,22 @@
+let bodyEl = document.querySelector(".body");
+const homeTitle = document.createElement("h1");
+homeTitle.innerHTML = "Mad Lib<br>Cheeseburger Mishap";
+const startBtn = document.createElement("button");
+startBtn.classList.add('btn')
+startBtn.innerHTML = "Begin";
+bodyEl.appendChild(homeTitle);
+bodyEl.appendChild(startBtn);
+
+startBtn.addEventListener('click', startLib);
+
+function startLib() {
+    homeTitle.innerHTML = "Noun";
+    startBtn.classList.add('hidden');
+    let Noun1 = document.createElement('input');
+    Noun1.classList.add('input');
+    let btn = document.createElement('button');
+    btn.innerHTML = "Next";
+    btn.classList.add('btn');
+    bodyEl.appendChild(Noun1);
+    bodyEl.appendChild(btn);
+}
