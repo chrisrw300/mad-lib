0471acf8fe16a806c52fb341627781ea27d80cd9
chrisrw300 <chrisrw300@gmail.com>
2022-04-24 21:59:27 -0600

    added variables, finised story

--- assets/script.js
+++ assets/script.js
@@ -10,18 +10,51 @@ bodyEl.appendChild(startBtn);
 startBtn.addEventListener('click', startLib);
 
 function startLib() {
-    homeTitle.innerHTML = "Noun";
-    startBtn.classList.add('hidden');
-    let Noun1 = document.createElement('input');
-    Noun1.classList.add('input');
-    let btn = document.createElement('button');
-    btn.innerHTML = "Next";
-    btn.classList.add('btn');
-    bodyEl.appendChild(Noun1);
-    bodyEl.appendChild(btn);
-    btn.addEventListener('click', enterValue);
-}
+    let bodyEl = document.querySelector(".body");
 
-function enterValue(Noun1) {
-    console.log(Noun1.innerHTML);
+    let day = prompt("Day of the Week");
+    day.classList.add('answer');
+    let bodyPart = prompt("Part of the Body");
+    bodyPart.classList.add('answer');
+    let restaurant = prompt("Name of Restaurant");
+    restaurant.classList.add('answer');
+    let smellAdj = prompt("Type of Smell");
+    smellAdj.classList.add('answer');
+    let technology = prompt("Type of Technology");
+    technology.classList.add('answer');
+    let friend = prompt("Name");
+    friend.classList.add('answer');
+    let show = prompt("TV Show Name");
+    show.classList.add('answer');
+    let friendActionIng = prompt("Action ending with ING");
+    friendActionIng.classList.add('answer');
+    let numberMinutes = prompt("Numbner");
+    numberMinutes.classList.add('answer');
+    let adj1 = prompt("Adjective");
+    adj1.classList.add('answer');
+    let adj2 = prompt("Adjective");
+    adj2.classList.add('answer');
+    let color = prompt("Type of Color");
+    let clothing = prompt("Piece of Clothing");
+    let verbEd1 = prompt("Verb Ending with ED");
+    let verbEd2 = prompt("Verb Ending with ED");
+    let verb1 = prompt("Verb");
+    let verbIng = prompt("Verb Ending with ING");
+    let publicTransport = prompt("Type of Public Transportation");
+    let adjLy1 = prompt("Adjective Ending with LY");
+    let verbEd3 = prompt("Verb Ending with ED");
+    let yell = prompt("Funny Phrase");
+    let verb2 = prompt("Verb");
+    let nounS = prompt("Noun Ending with S");
+    let bodyPart2 = prompt("Part of the Body");
+    let food = prompt("Food");
+    let verb3 = prompt("Verb");
+
+
+    let madLib = document.createElement("p");
+    
+    madLib.innerHTML = "Someone stole my cheeseburger yesterday. It was a " + day + " afternoon. The rain was so bad that I couldn't see my " + bodyPart + ". I went to " + restaurant + " to get a cheeseburger and keep dry. When I got inside, it smelled " + smellAdj + " and my stomach grumbled. Once I ordered my cheeseburger, I stood by to pick it up. My " + technology + " began to ring. It was my friend, " + friend + ". They wanted to update me on the latest episode of " + show + ". I turned around so I could hear them better. They wouldn't stop " + friendActionIng + " even when I asked them to stop. " + numberMinutes + " minutes passed by and I still haven't gotten my order. I went up to the counter and asked where my cheeseburger was. They looked at my receipt and told me that a " + adj1 + adj2 + " man with a " + color + clothing + " came up and " + verbEd1 + " my cheeseburger. I " + verbEd2 + " and " + verb1 + " out of the restaurant to see if I could find the culprit. I saw him " + verbIng + " my cheeseburger as he waited for the " + publicTransport + ". I ran as " + adjLy1 + " as I could before he got away for good. He " + verbEd3 + " and yelled, " + yell + "! I quickly snatched my cheeseburger back and " + verb2 + " on the ground. He looked at me with " + nounS + " in his " + bodyPart2 + ". It turns out he couldn't afford to get any " + food + " until his next paycheck. I " + verb3 + " back to " + restaurant + "'s and got him a whole meal. He thanked me and went on his way. I told him to keep in touch just in case he was in the area. We became good friends after that Cheeseburger Mishap.";
+
+    bodyEl.appendChild(madLib);
+    
 }
